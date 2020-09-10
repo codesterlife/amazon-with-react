72497c0051f559386bd78b15d15da2b8d942afc9
codester <codesterlife@gmail.com>
2020-09-10 20:01:24 +0530

    Initial commmit to repo

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAKIu4N62J1H8CwQXvzTM0sQmK96jLhFvc",
+    authDomain: "amzn-by-abhi.firebaseapp.com",
+    databaseURL: "https://amzn-by-abhi.firebaseio.com",
+    projectId: "amzn-by-abhi",
+    storageBucket: "amzn-by-abhi.appspot.com",
+    messagingSenderId: "1026296978248",
+    appId: "1:1026296978248:web:cf60593cc7a5ab161d317d",
+    measurementId: "G-VRQZ8PCCQR"
+  };
