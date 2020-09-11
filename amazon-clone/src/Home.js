26522a0a2e62764db96ed4108c0c9690156dafc1
codester <codesterlife@gmail.com>
2020-09-11 15:31:25 +0530

    checkout functionality created aalong with usage of React Context API. Day 2 of Build Complete.

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -13,6 +13,7 @@ function Home() {
         />
         <div className="home__row">
           <Product
+            id="1"
             title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
             price={499}
             rating={5}
@@ -21,34 +22,39 @@ function Home() {
         </div>
         <div className="home__row">
           <Product
+            id="2"
             title="OnePlus 8 Pro (Onyx Black 8GB RAM+128GB Storage)"
-            price={(54, 999)}
+            price={54999}
             rating={4}
             image="https://m.media-amazon.com/images/I/61YSMhOd5EL._AC_UL640_FMwebp_QL65_.jpg"
           />
           <Product
+            id="3"
             title="Audio-Technica ATH-M50x Over-Ear Headphones ..."
-            price={(11, 110)}
+            price={11110}
             rating={5}
             image="https://m.media-amazon.com/images/I/71WNWRLJCsL._AC_UY436_FMwebp_QL65_.jpg"
           />
           <Product
+            id="4"
             title="Logitech G 304 Lightspeed Wireless Gaming Mouse - Black ..."
-            price={(3, 295.0)}
+            price={3295}
             rating={5}
             image="https://m.media-amazon.com/images/I/61n5dYX+OhL._AC_UY436_FMwebp_QL65_.jpg"
           />
         </div>
         <div className="home__row">
           <Product
+            id="5"
             title='Razer Rogue 13.3" Backpack V2- Black ...'
-            price={(4, 999)}
+            price={4999}
             rating={4}
             image="https://m.media-amazon.com/images/I/51xIf7Q818L._AC_UL640_FMwebp_QL65_.jpg"
           />
           <Product
+            id="6"
             title="Razer Doubleshot PBT Keycap Upgrade Set for Mechanical & Optical Keyboard ..."
-            price={(2, 899.0)}
+            price={2899}
             rating={4}
             image="https://m.media-amazon.com/images/I/611-ah5Bh9L._AC_UY436_FMwebp_QL65_.jpg"
           />
--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-function Product({ title, price, image, rating }) {
+function Product({ id, title, price, image, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  console.log("This is the basket >>>", basket);
+
+  const addToBasket = () => {
+    //dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className="product">
       <div className="product__info">
         <p>{title}</p>
         <p className="product__price">
-          <small>Rs.</small>
+          <small>₹</small>
           <strong>{price}</strong>
         </p>
         <div className="product__rating">
@@ -19,7 +36,7 @@ function Product({ title, price, image, rating }) {
         </div>
       </div>
       <img src={image} alt="" />
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
